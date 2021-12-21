1a8626579e2fe25d6b4a262ccb827082362d4475
Kruthika R <65350577+Kruthika-ML@users.noreply.github.com>
2021-12-21 09:33:43 +0530

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,18 @@
+public class Switch {
+    public static void main(String[] args) {
+        int SwitchValue = 2;
+
+        switch (SwitchValue){
+            case 1:
+                System.out.println("Value is 1");
+                break;
+
+            case 2:
+                System.out.println("Value is 2");
+                break;
+
+            default:
+                System.out.println("Others");
+        }
+    }
+}
